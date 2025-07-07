ed61a3fcb71141517194cfd6379c8e251a4ad8aa
Bharati <devanibharati1998@gmail.com>
2025-07-07 21:49:17 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,7 +9,7 @@ import Messagebox from './Messagebox.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* // <App /> */}
-    <Increament/>
+    <Increament />
     <Messagebox/>
-  </StrictMode>,
+  </StrictMode>
 )
